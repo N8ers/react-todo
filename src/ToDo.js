5d6736b63a21586e8cead324c6207975915f5333
n8ers <nathanraysheryak@gmail.com>
2019-10-05 17:33:27 -0400

    added edit and checbox functionality

--- src/ToDo.js
+++ src/ToDo.js
@@ -5,20 +5,83 @@ class ToDo extends Component {
     
     constructor(props){
         super(props)
+        this.state = ({
+            editMode: false,
+            todo: this.props.item,
+        })
         this.handleRemove = this.handleRemove.bind(this)
+        this.handleToggleEdit = this.handleToggleEdit.bind(this)
+        this.handleSaveEdit = this.handleSaveEdit.bind(this)
+        this.handleCompletion = this.handleCompletion.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
 
     handleRemove() {
         this.props.remove(this.props.id)
     }
 
+    handleToggleEdit() {
+        this.setState({editMode: !this.state.editMode})
+    }
+
+    handleCompletion(){
+        this.props.toggleStatus(this.props.id)
+    }
+
+    handleChange(e) {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+        console.log('handleChange: ', this.state.todo)
+    }
+
+    handleSaveEdit(e){
+        e.preventDefault()
+
+        if (this.state.todo.length < 1){
+            alert("Hey! That was empty!")
+        } else {
+            this.props.edit(this.props.id, this.state.todo)
+        }
+
+        this.handleToggleEdit()
+    }
+
     render() {
-        return(
-            <div className="ToDoItem">
-                <span className="ToDoContent">{this.props.item}</span>
-                <button className="ToDoX" onClick={this.handleRemove}>x</button>
-            </div>
-        )
+        if(this.state.editMode === false){
+            return(
+                <div className="ToDoItem">
+                    <span>
+                        <input  type="checkbox" onClick={this.handleCompletion} checked={this.props.status} /> 
+                        <span className="ToDoContent">{this.props.item}</span>
+                    </span>
+                    <span>
+                        <button className="ToDoEdit" onClick={this.handleToggleEdit}>/</button>
+                        <button className="ToDoX" onClick={this.handleRemove}>x</button>
+                    </span>
+                </div>
+            )
+        } else {
+            return(
+                <form className="ToDoItem" onSubmit={this.handleSaveEdit}>
+                    <input 
+                        className="ToDoContent" 
+                        placeholder={this.props.item} 
+                        value={this.state.todo}
+                        name='todo'
+                        onChange={this.handleChange}   
+                    />
+                    <span>
+                        <button className="ToDoEditSave">save</button>
+                        <button 
+                            className="ToDoX" 
+                            onClick={this.handleRemove}
+                        >x</button>
+                    </span>
+                </form>
+            )
+        }
+       
     }
 }
 
--- src/ToDoContainer.js
+++ src/ToDoContainer.js
@@ -10,12 +10,14 @@ class ToDoContainer extends Component {
         this.state = {
             todos: [
                 {todoItem: 'walk dog', completed: false, id: '1'}, 
-                {todoItem: 'throw rocks', completed: false, id: '2'}, 
+                {todoItem: 'throw rocks', completed: true, id: '2'}, 
                 {todoItem: 'eat garbage', completed: false, id: '3'}
             ],
         }
         this.removeTodo = this.removeTodo.bind(this)
         this.addTodo = this.addTodo.bind(this)
+        this.toggleCompletionStatus = this.toggleCompletionStatus.bind(this)
+        this.editTodo = this.editTodo.bind(this)
     }
 
     removeTodo(id){
@@ -24,8 +26,27 @@ class ToDoContainer extends Component {
         })
     }
 
+    toggleCompletionStatus(id){
+        const updateStatus = this.state.todos.map(todo => {
+            if(todo.id === id){
+                return {...todo, completed: !todo.completed}
+            }
+            return todo;
+        })
+        this.setState({todos: updateStatus})
+    }
+
+    editTodo(id, editedTodoItem) {
+        const updateItem = this.state.todos.map(todo => {
+            if(todo.id === id){
+                return {...todo, todoItem: editedTodoItem}
+            } 
+            return todo;
+        })
+        this.setState({todos: updateItem})
+    }
+
     addTodo(newTodo){
-        console.log(newTodo)
         this.setState({
             todos: [...this.state.todos, newTodo]
         })
@@ -41,7 +62,10 @@ class ToDoContainer extends Component {
                             id={todo.id} 
                             item={todo.todoItem} 
                             remove={this.removeTodo}
+                            edit={this.editTodo}
+                            toggleStatus={this.toggleCompletionStatus}
                             key={todo.id}
+                            status={todo.completed}
                     />)
                 }
             </ul>

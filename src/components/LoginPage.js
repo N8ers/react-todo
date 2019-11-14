a66930b9c2b170f835e05cfd76e0029f1e92132d
n8ers <nathanraysheryak@gmail.com>
2019-11-13 19:20:18 -0500

    do some styling

---
+++ src/components/LoginPage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import moment from 'moment'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCog } from '@fortawesome/free-solid-svg-icons'
+import '../styles/ToDoHeader.css'
+
+class ToDoHeader extends Component {
+
+  render() {
+    const date = moment().format('dddd, MMMM Do');
+
+    return (
+      <div className="ToDoHeader">
+        <FontAwesomeIcon className="cog-icon" icon={faCog} />
+        <h1>todo</h1>
+        <h5>{date}</h5>
+      </div>
+    );
+  }
+}
+
+export default ToDoHeader;
--- src/components/ToDoHeader.js
+++ src/components/ToDoHeader.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import moment from 'moment'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faCog } from '@fortawesome/free-solid-svg-icons'
-
+import { faCog } from '@fortawesome/free-solid-svg-icons'
 import '../styles/ToDoHeader.css'
 
 class ToDoHeader extends Component {
@@ -12,7 +11,7 @@ class ToDoHeader extends Component {
 
         return (
             <div className="ToDoHeader">
-                {/* <FontAwesomeIcon className="cog-icon" icon={faCog} /> */}
+                <FontAwesomeIcon className="cog-icon" icon={faCog} />
                 <h1>todo</h1>
                 <h5>{date}</h5>
             </div>
